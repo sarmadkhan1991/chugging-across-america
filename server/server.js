c895add32dfbc3fa23367c1fd6af066152236ab9
Katrina Behnken <katrina.behnken@gmail.com>
2020-02-04 11:58:36 -0700

    imported authController.js and added /api/auth routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
-
+const session = require('express-session');
+const massive = require('massive');
 const app = express();
 
+const authCtrl = require('./controllers/authController');
+
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
+// massive(CONNECTION_STRING).then(db => {
+//     app.set('db', db);
+//     console.log('db connected');
+// });
+
 app.use(express.json());
+app.use(
+    session({
+      resave: true,
+      saveUninitialized: false,
+      secret: SESSION_SECRET,
+    })
+  );
+  
+app.post('/api/auth/register', authCtrl.register);
+app.post('/api/auth/login', authCtrl.login);
+app.post('/api/auth/logout', authCtrl.logout);
 
 app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}...`));
