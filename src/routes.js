a50eeaaa2890398b91393de7c68ed2f033e40946
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-02-04 15:43:39 -0700

    Initial map component

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Map from './Components/Map/Map';
 
 export default (
     // add routes in this switch.
     // to add routed import your component and add path and component attribut to the route. See example below
     <Switch>
         {/* <Route component={your component} path='your component path' /> */}
-        
+        <Route component={Map} path='/map' />
     </Switch>
 )
