6fb0fd7440f247a334db15553fd25282731d3536
Katrina Behnken <katrina.behnken@gmail.com>
2020-02-10 16:35:55 -0700

    changed Welcome to Cheers on line 92

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -89,7 +89,7 @@ class Header extends Component {
                     {user.username ?
                         (
                             <div>
-                                Cheers, {user.username}! | 
+                                Cheers, {user.username} | 
                                 <button onClick={this.logout}>
                                     Logout
                                 </button>
