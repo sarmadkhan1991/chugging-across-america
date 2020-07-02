24b5d2d40082803ab26598857b69d00a1d576c93
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2020-07-02 12:53:14 -0700

    opacity issue resolved and changed port to 5000

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -17,13 +17,13 @@ module.exports = function (app) {
 
     app.use(
         proxy("/api", {
-            target:"http://localhost:4000"
+            target:"http://localhost:5000"
         })
     );
 
     app.use(
         proxy("/user", {
-            target:"http://localhost:4000"
+            target:"http://localhost:5000"
         })
     );
 }
